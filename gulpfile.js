fae61336452e929bf4ef4011dce898987556d04f
allenca <allen.ca.chan@gmail.com>
2017-02-02 16:38:04 -0500

    005
    Edits to correct the page display and referencing files

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,22 @@
-"use strict";
 
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 
-gulp.task("scripts-scss", function(){
-	gulp.src("./src/scss/*.scss")
-	.pipe(concat("all.scss"))
-	.pipe(sass())
+gulp.task("scss", function(){
+	gulp.src("./src/scss/app.scss")
+	.pipe(sass().on("error", sass.logError))
 	.pipe(uglifycss({
 		"maxLineLen": 100000,
 		"uglyComments": true
     }))
-	.pipe(rename("concated.css"))
     .pipe(gulp.dest("./dist/css/"));
 // tells where to look for files, concats, uglifies, renames, pipes to dist folder
 // need to download the plugins before running them
 });
 
 gulp.task("watch"),function(){
-	gulp.watch("src/scss/*.scss",["scripts-scss"]);
+	gulp.watch("src/scss/*.scss",["scss"]);
 };
