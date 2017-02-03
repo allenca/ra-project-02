476afda53ca20a9d43f6a7d674e1740799128dce
allenca <allen.ca.chan@gmail.com>
2017-02-03 15:18:55 -0500

    007
    Edit to page styling
    Continue to edit javascript files, working on getting carousel to show

--- src/CatalogView.js
+++ src/CatalogView.js
@@ -24,7 +24,24 @@ export default class CatalogView{
         belonging to that class) - see line 88 below.
          */
 
-        this.carousel = document.getElementsByClassName("container");
+       this.carousel = document.getElementsByClassName("container");
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1240:{
+            items:4
+        }
+    }
+  })
     }
 
     addProductsToCarousel(products){
